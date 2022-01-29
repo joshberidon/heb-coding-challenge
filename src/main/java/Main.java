16cc7766c3def9bd023f02446522668affad5ef5
JOSH-TOWER\Josh <jberidon@gmail.com>
2022-01-29 15:44:02 -0600

    Add javalin hello world

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/images", ctx -> ctx.result("Hello World"));
+        app.get("/images/query", ctx -> ctx.result("Hello World"));
+        app.get("/images/id", ctx -> ctx.result("Hello World"));
 
     }
 }
