e4ee36a0afed85ca106d0982dc3eeb31620dafc2
JOSH-TOWER\Josh <jberidon@gmail.com>
2022-01-30 14:05:59 -0600

    Remove okhttp gist example code

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -15,7 +15,6 @@ import java.util.Objects;
 public class HttpClient {
     private final OkHttpClient client;
     private final Moshi moshi = new Moshi.Builder().build();
-    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
     private final JsonAdapter<ResultEnvelope> tagsJsonAdapter = moshi.adapter(ResultEnvelope.class);
 
     public HttpClient() {
@@ -55,31 +54,5 @@ public class HttpClient {
 
         }
         return null;
-    }
-
-
-    public void run() throws Exception {
-        Request request = new Request.Builder()
-                .url("https://api.github.com/gists/c2a7c39532239ff261be")
-                .build();
-        try (Response response = client.newCall(request)
-                .execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-            Gist gist = gistJsonAdapter.fromJson(response.body()
-                    .source());
-
-            for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
-                System.out.println(entry.getKey());
-                System.out.println(entry.getValue().content);
-            }
-        }
-    }
-
-    static class Gist {
-        Map<String, GistFile> files;
-    }
-
-    static class GistFile {
-        String content;
     }
 }
