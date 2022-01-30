af3cf75a9e339937b90f12f22020fea10b8a18ef
JOSH-TOWER\Josh <jberidon@gmail.com>
2022-01-29 19:40:02 -0600

    Add basic sqlite db & tables, insert tag, insert image with url

---
+++ src/main/java/Database.java
@@ -0,0 +1,102 @@
+import java.sql.*;
+import java.util.List;
+
+public class Database {
+    private Connection conn;
+
+    public Database(String s) {
+        createNewDatabase(s);
+        dropTables();
+        createImageTable();
+        createTagsTable();
+        createImageTagJunctionTable();
+    }
+
+    private void createNewDatabase(String fileName) {
+        String url = "jdbc:sqlite:" + fileName;
+
+        try {
+            conn = DriverManager.getConnection(url);
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+            }
+
+        } catch (SQLException e) {
+            //todo
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private ResultSet execute(String sql) {
+        //make sure we  have a connection
+        if (conn == null) {
+            throw new RuntimeException("No database connection");
+        }
+        try (Statement statement = conn.createStatement()) {
+            statement.execute(sql);
+            return statement.getResultSet();
+        } catch (SQLException e) {
+            //todo
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private void dropTables() {
+        execute("DROP TABLE IF EXISTS images");
+        execute("DROP TABLE IF EXISTS tags");
+        execute("DROP TABLE IF EXISTS image_tag_junction");
+    }
+
+    private void createImageTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS images (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	label text NOT NULL,\n"
+                + "	tags text,\n"
+                + "	image blob,\n"
+                + "	url text\n"
+                + ");";
+
+        execute(sql);
+    }
+
+    private void createTagsTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS tags (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	tag text NOT NULL,\n"
+                + " UNIQUE(tag)"
+                + ");";
+
+        execute(sql);
+    }
+
+    private void createImageTagJunctionTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS image_tag_junction (\n"
+                + "	image integer NOT NULL,\n"
+                + "	tag integer NOT NULL,\n"
+                + " FOREIGN KEY(image) REFERENCES images(id), \n"
+                + " FOREIGN KEY(tag) REFERENCES tags(id) \n"
+                + ");";
+        execute(sql);
+    }
+
+    private void createTag(String tag) {
+        String sql = String.format("INSERT OR IGNORE INTO tags (tag) " +
+                "values ('%s');", tag.toLowerCase());
+        System.out.println(sql);
+        execute(sql);
+    }
+
+
+    public void addImageByUrl(String url, String label, List<String> tags) {
+        //add tag if does not exist
+        tags.forEach(this::createTag);
+        String sql = String.format(
+                "INSERT INTO IMAGES " +
+                        "(label,tags,url)" +
+                        "values " +
+                        "('%s','%s','%s');",
+                label, String.join(",", tags), url);
+        execute(sql);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import io.javalin.Javalin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7070);
@@ -8,6 +12,12 @@ public class Main {
         app.get("/images", ctx -> ctx.result("Hello World"));
         app.get("/images/query", ctx -> ctx.result("Hello World"));
         app.get("/images/id", ctx -> ctx.result("Hello World"));
+        app.post("/images", ctx -> ctx.result(""));
 
+
+        Database database = new Database("heb.sqlite");
+
+        database.addImageByUrl("url", "label", Arrays.asList("cat", "dog"));
     }
+
 }
